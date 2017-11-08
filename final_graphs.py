dc4e8a1d9f966ba50dd20f307a019471d6d72b3d
Miroslav Šerý <athes01@gmail.com>
2017-11-08 17:52:10 +0100

    graphing

---
+++ final_graphs.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+import numpy as np
+
+year = 2013
+
+def loadCsvFile(filename: str):
+    data = []
+    with open('data/{}.csv'.format(filename)) as csv_file:
+        for line in csv_file:
+            data.append(line.split(';'))
+
+    return data
+
+
+data_deaths = loadCsvFile('region_deaths')
+data_counts = loadCsvFile('region_counts')
+data_areas = loadCsvFile('region_areas')
+
+data_deaths.sort(key=lambda tup: tup[0], reverse=False)
+data_counts.sort(key=lambda tup: tup[0], reverse=False)
+data_areas.sort(key=lambda tup: tup[0], reverse=False)
+
+assert len(data_deaths) == len(data_counts) == len(data_areas)
+
+values = [] # (name, density, percentage of deaths)
+
+for i in range(len(data_deaths)):
+    district_name = data_counts[i][0]
+    district_area = float(data_areas[i][1])
+    district_count = float(data_counts[i][year - 2004])
+    district_death = float(data_deaths[i][year - 2004])
+
+    # by a year
+    density = district_count / district_area
+    percent_death = district_death / district_count
+
+    values.append((district_name,density, percent_death))
+
+values.sort(key=lambda tup: tup[2], reverse=False)
+
+# print(values)
+
+y_pos = np.arange(len(values))
+
+plt.bar(y_pos, list(map(lambda tup: tup[1],values)), align='center', alpha=0.5)
+plt.xticks(y_pos,list(map(lambda tup: tup[0], values)), rotation='vertical')
+plt.ylabel('Okresy')
+plt.title('Okresy v ČR')
+
+plt.show()
